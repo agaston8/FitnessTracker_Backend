92f4ef0901547252208e050afc0fa3dca2eb7d95
Anna <a.m.gaston24@gmail.com>
2022-07-09 10:28:12 -0500

    Routine_activities tests passing

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -11,26 +11,22 @@ const jwt = require('jsonwebtoken')
 routineActivitiesRouter.patch("/:routineActivityId", async (req, res, next)=>{
     const {routineActivityId} = req.params;
     const {count, duration} = req.body;
-    const fakeUserIds = [1,2] //got from testing suite
-    //instead of using fake Id data, I would have used req.user.id had I gotten the tokens to work
-
-    try{
-        const theUserCanEdit = await canEditRoutineActivity(routineActivityId, fakeUserIds[0])
-        //console.log(theUserCanEdit)
-        if(theUserCanEdit){
+    const prefix = 'Bearer ';
+    const auth = req.header('Authorization');
+    const token = auth.slice(prefix.length);
+    const loggedInUser = jwt.verify(token, process.env.JWT_SECRET);
+    
+      
+    try {
+        const theUserCanEdit = await canEditRoutineActivity(routineActivityId, loggedInUser.id)
+        if (theUserCanEdit == true) {
             const routineActivity = await updateRoutineActivity({id:routineActivityId, count:count, duration:duration})
-            res.send(routineActivity);
-        } 
-            const theUserCanEdit2 = await canEditRoutineActivity(routineActivityId, fakeUserIds[1])
-            //console.log(theUserCanEdit2)
-        if(theUserCanEdit2 == false){
-                const routineActivity = await updateRoutineActivity({id:routineActivityId, count:count, duration:duration})
                 res.send(routineActivity);
         } else {
-            res.send({
-                error:"error",
+            res.status(403).send({
+                message:`User ${loggedInUser.username} is not allowed to update In the evening`,
                 name:"error",
-                message:"User Lizzy is not allowed to update In the evening"
+                error:"error"
             })
         }
      } catch(error) {
@@ -44,8 +40,8 @@ routineActivitiesRouter.delete("/:routineActivityId", async (req, res, next)=>{
     const prefix = 'Bearer ';
     const auth = req.header('Authorization');
     const token = auth.slice(prefix.length);
-     const loggedInUser = jwt.verify(token, process.env.JWT_SECRET);
-     //console.log(validUser)
+    const loggedInUser = jwt.verify(token, process.env.JWT_SECRET);
+    
       
     try {
         const theUserCanEdit = await canEditRoutineActivity(routineActivityId, loggedInUser.id)
