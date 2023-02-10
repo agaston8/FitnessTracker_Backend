b46ec853fdfa4fb711076271a28a200566591b99
Anna <a.m.gaston24@gmail.com>
2023-02-09 20:01:42 -0600

    triallinge merge

--- db/seedData.js
+++ db/seedData.js
@@ -64,8 +64,9 @@ async function createTables() {
         CREATE TABLE activities (
           id SERIAL PRIMARY KEY,
           name VARCHAR(255) UNIQUE NOT NULL, 
-          description TEXT NOT NULL
-        );/
+          description TEXT NOT NULL,
+          image VARCHAR(255)
+        );
         CREATE TABLE routines (
           id SERIAL PRIMARY KEY,
           "creatorId" INTEGER REFERENCES users(id),
